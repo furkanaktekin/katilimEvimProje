577c38ee1d54dc561539f690a9be8d781c4c4470
furkanaktekin <furkan_aktekin@hotmail.com>
2020-12-16 00:17:58 +0300

    Katılımevim Proje

---
+++ Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" Inherits="katilimEvimProje.Default" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+        <meta charset="UTF-8">
+	<title>Default</title>
+</head>
+<body>
+	<form id="form1" runat="server">
+            <div style="width:500px;margin-right:auto;margin-left:auto">
+                   <br/>
+                <img src="https://www.katilimevim.com.tr/wp-content/uploads/katilimevim-logoV2.svg"/>
+                <br/>
+                <br/>
+                 <br/>
+                <br/>
+                <asp:FileUpload id="FileUpload1" runat="server" style="cursor:pointer"></asp:FileUpload> 
+                
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                <asp:Button id="UploadButton" Text="Dosyayı Oku" OnClick="UploadButton_Click" runat="server" style="height:40px;border:solid 1px #0055B8;background-color:#0055B8;color:#FF6B00;cursor:pointer;font-size:20px;border-radius:5px"></asp:Button> 
+                
+                <br/>
+                <br/>
+                
+                <asp:Label id="UploadStatusLabel" runat="server"></asp:Label>  
+                
+            </div>
+	</form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+
+namespace katilimEvimProje
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+
+            if (FileUpload1.HasFile)
+            {
+
+                string[] textLineNumbersUp;
+                string[] textLineNumbersDown;
+
+                int[] numbersUp;
+                int[] numbersDown;
+
+                int sonuc=0;
+
+                int toplam=0;
+
+                string secilenler="";
+
+                List<string> list = new List<string>();
+
+                // SEÇİLEN DOSYANIN HER BİR SATIRINI OKUYUP LİST'E ATTIM.
+                StreamReader reader = new StreamReader(FileUpload1.FileContent);
+                do
+                {
+                    list.Add(reader.ReadLine());
+                }
+                while (reader.Peek() != -1);
+                reader.Close();
+
+
+                // HER SATIRI BOŞLUK İFADESİNE GÖRE PARÇALAYIP STRİNGLERİ İNT DİZİYE ÇEVİRDİM.
+                // DAHA SONRA BU İNT DİZİYİ KÜÇÜKTEN BÜYÜĞE SIRALADIM.
+                // BİR ALT SATIRI DA AYNI ŞEKİLDE PARÇALAYIP İNT DİZİYE ÇEVİRİP SIRALADIM.
+                // ÜST SATIRIN MİN DEĞERİ ALT SATIRIN MİN DEĞERİNDEN BÜYÜKSE O ELEMANI SEÇTİM.
+                for(int i=0;i<list.Count;i++)
+                {
+                    textLineNumbersUp = list[i].Split(' ');
+
+                    numbersUp = Array.ConvertAll<string, int>(textLineNumbersUp, int.Parse);
+
+                    Array.Sort(numbersUp);
+
+                    if (i + 1 == list.Count)
+                        sonuc = numbersUp[0];
+                    else
+                    {
+                        for (int j = 0; j < numbersUp.Length; j++)
+                        {
+                            sonuc = numbersUp[j];
+
+                            
+                            textLineNumbersDown = list[i + 1].Split(' ');
+
+                            numbersDown = Array.ConvertAll<string, int>(textLineNumbersDown, int.Parse);
+
+                            Array.Sort(numbersDown);
+
+                            if (sonuc > numbersDown[0])
+                                break;
+                    
+                        }
+                    }
+                    
+                    secilenler += sonuc+" + ";
+
+                    toplam = toplam + sonuc;
+                }
+
+                secilenler = secilenler.Remove(secilenler.Length - 3);
+
+                UploadStatusLabel.Text = "Minimal path is: "+secilenler+" = "+toplam;
+            }
+            else
+            {
+                UploadStatusLabel.Text = "Lütfen Önce Dosya Seçip Daha Sonra Dosyayı Oku Butonuna Basınız";
+            }
+        }
+    }
+}
